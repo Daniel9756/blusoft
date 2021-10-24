282211495cf68fee35b70cdc7505d4a8f901f279
Daniel9756 <aniagorumg@gmail.com>
2021-10-25 00:23:34 +0100

    contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,76 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { Col, Row, Container } from "react-bootstrap"
-function Contact() {
+import { MdLocationOn, MdPhone } from "react-icons/md"
+import { IoIosMail } from "react-icons/io"
+import emailjs from 'emailjs-com';
+import { useForm } from "react-hook-form";
+import { FaHome, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+
+const config: string | undefined = (process.env.REACT_APP_EMAIL_SERVICE_ID as string);
+const config1: string | undefined = (process.env.REACT_APP_EMAIL_TEMPLATE_ID as string);
+const config2: string | undefined = (process.env.REACT_APP_EMAIL_USER_ID as string);
+
+function Contact({ process }: any) {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data: any) => {
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE_ID, process.env.REACT_APP_EMAIL_TEMPLATE_ID, data, process.env.REACT_APP_EMAIL_USER_ID)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+
+
     return (
         <Container>
             <Row>
-                <Col md={4}>
-                    Here is contact page of this website
-                </Col>
-                <Col md={8}>
-                    Here is contact page of this website
-                </Col>
 
+                <Col md={8}>
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="form">
+
+                        <h3 style={{ marginLeft: 16 }}>GET IN TOUCH</h3>
+                        <em style={{ marginLeft: 16, marginBottom: "32px" }}> Feel free to drop us a ine below</em>
+                        <div className="formelement">
+                            <input {...register("Name", { required: true, maxLength: 40 })} className="input" />
+
+                        </div>
+                        <div className="formelement">
+                            <input {...register("email", { required: true })} className="input" />
+
+                        </div>
+                        <div className="formelement">
+                            <textarea  {...register("message", { min: 30, })} className="input" />
+
+                        </div>
+                        <div className="formelement">
+
+                            <button type="submit" className="emailbtn" >SEND</button>
+                        </div>
+                    </form>
+                </Col>
+                <Col md={4} >
+                    <div className="location" style={{ zIndex: 100 }}>
+                        <h2>Contact Us</h2>
+                        <div className="address">
+                            <MdLocationOn style={{ fontSize: "68px", color: "#7c3626" }} />
+                            <p className="addresstext">No1 Ikemba Street Suncity Layout, Alulu Road, Alulu Nike, Enugu State</p>
+                        </div>
+                        <div className="address">
+                            <IoIosMail style={{ fontSize: "38px", color: "#7c3626" }} />   <p className="addresstext">bluesofttech.js@gmail.com</p>
+                        </div>
+                        <div className="address">
+                            <MdPhone style={{ fontSize: "38px", color: "#7c3626" }} />   <p className="addresstext">+234 906 774 4732</p>
+                        </div>
+                        <hr />
+                        <div className="address">
+                            <p className="addresstext"></p> <div> <FaGithubSquare style={{ fontSize: "38px", color: "#7c3626" }} /> <FaLinkedin style={{ fontSize: "38px", color: "#7c3626" }} /></div>
+                        </div>
+                    </div>
+                </Col>
             </Row>
 
         </Container>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,6 +4,7 @@ import "./Comp.css"
 import { useQuery, gql, useMutation } from "@apollo/client";
 import { GET_AN_AUTHOR } from "./Gql"
 import { Row, Col, Container, Image } from 'react-bootstrap';
+import { FaHome, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 
 type TParams = { id: string };
@@ -11,7 +12,7 @@ type TParams = { id: string };
 function Profile({ match }: RouteComponentProps<TParams>) {
 
     const id = match.params.id
-  
+
     const { loading, error, data } = useQuery(GET_AN_AUTHOR, {
         variables: { id }
     });
@@ -29,35 +30,33 @@ function Profile({ match }: RouteComponentProps<TParams>) {
                 <Col md={6}>
                     <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column" }}>
                         <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column" }}>
-                       
+
                             <div style={{ marginTop: 35 }}>
                                 <Image src="https://res.cloudinary.com/lectua/image/upload/c_fill,h_294,q_88,w_283,x_241,y_200/v1634908595/person_lrod2j.jpg" rounded />
                             </div>
                         </div>
-                        <div>
-                            <p className="h3">{data?.getOneAuthor?.fullname}</p>
-                        </div>
-                        <div>
-                            <p className="h3">{data?.getOneAuthor?.occupation}</p>
-                        </div>
-                        <div>
-                            <p className="h3">{data?.getOneAuthor?.email}</p>
+                        <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column" }}>
+                            <em className="h3">{data?.getOneAuthor?.fullname}</em>
+
+                            <em className="h3">{data?.getOneAuthor?.occupation}</em>
+
+                            <em className="h3">{data?.getOneAuthor?.email}</em>
                         </div>
                     </div>
                 </Col>
-                <Col md={6}>
-                    {/* <pre>{error.graphQLErrors.map(({ message}, i) => (
-                        <span key={i} className="error">{message}</span>
-                    ))}
-                    </pre> */}
+                <Col md={6} style={{ marginTop: "32px" }}>
+
                     <div>
-                        <h2 className="h3">{data?.getOneAuthor?.bio}</h2>
+                        <p className="h3">{data?.getOneAuthor?.bio}</p>
                     </div>
                     <hr />
-                    <div>
-                        <h2 className="h3">{data?.getOneAuthor?.role}</h2>
+                    <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "baseline", }}>
+                        <h4 className="h3">Role:</h4>           <p className="h3" style={{ marginTop: "22px", fontFamily: "serif", marginLeft: "22px" }}>{data?.getOneAuthor?.role}</p>
                     </div>
-
+                    <div>
+                        <h2>Social</h2>
+                        <a href="https://github.com/Daniel9756" target="_blank"><FaGithubSquare style={{ fontSize: "38px" }} /></a> <a href="https://www.linkedin.com/in/corneliuseze/" target="_blank"><FaLinkedin style={{ fontSize: "38px" }} /></a>
+                    </div>
                 </Col>
 
 
